afaa64ac6e8cc1ba88d18939a959d644cb6bf4e1
marina <marinaaltesmagret@gmail.com>
2021-06-05 09:20:43 +0200

    Iteration 1

---
+++ src/components/IdCard/IdCard.jsx
@@ -0,0 +1,18 @@
+import './IdCard.css';
+
+const IdCard = (props) => {
+    const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return (
+        <div className="IdCard">
+            <img src={props.picture} alt="card"/>
+            <p><strong>First name: </strong> {props.firstName}</p>
+            <p><strong>Last name: </strong> {props.lastName}</p>
+            <p><strong>Gender: </strong> {props.gender}</p>
+            <p><strong>Height: </strong> {props.height/100}m</p>
+            <p><strong>Birth: </strong> {days[props.birth.getDay()-1]} {months[props.birth.getMonth()]} {props.birth.getDate()} {props.birth.getFullYear()}</p>
+        </div>
+    )
+}
+
+export default IdCard;
